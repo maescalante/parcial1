723c1f4a283f05dac924df5299f228b273b19eb7
maescalante <ma.escalante@uniandes.edu.co>
2020-09-22 11:57:31 -0500

    mil correcciones

--- main.js
+++ main.js
@@ -2,15 +2,22 @@ const url =
   "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
 let title = document.getElementById("tit");
 let content = document.getElementById("var");
-let items=document.getElementById("items");
-
+let items = document.getElementById("items");
+let burgersContent = document.getElementById("Burgers");
+let tacosContent = document.getElementById("Tacos");
+let saladsContent = document.getElementById("Salads");
+let dessertsContent = document.getElementById("Desserts");
+let drinksContent = document.getElementById("Drinks");
+let carrito = document.getElementById("car");
+carrito.addEventListener("click", function(){pedido()})
 burgers = [];
 tacos = [];
 salads = [];
 desserts = [];
 drinks = [];
-productos=[]
-total=0
+productos = [];
+total = 0;
+
 let peticion = (url) => {
   return new Promise((resolve, reject) => {
     let req = new XMLHttpRequest();
@@ -42,68 +49,156 @@ peticion(url).then((resp) => {
       ? (drinks = obj.products)
       : 0;
   });
-  render("Burgers")
-  
+  poner("Burgers", burgers);
+  burgersContent.addEventListener("click", function () {
+    poner("Burgers", burgers);
+  });
+  tacosContent.addEventListener("click", function () {
+    poner("Tacos", tacos);
+  });
+  saladsContent.addEventListener("click", function () {
+    poner("Salads", salads);
+  });
+  dessertsContent.addEventListener("click", function () {
+    poner("Desserts", desserts);
+  });
+  drinksContent.addEventListener("click", function () {
+    poner("Drinks & Sides", drinks);
+  });
+  items.addEventListener("click", function () {
+    addCart();
+  });
 });
 
-function render(lista) {
- 
-  lista === "Burgers"? poner("Burgers", burgers):  lista === "Tacos"? poner("Tacos", tacos) :  lista === "Salads"? poner("Salads", salads) :  lista === "Dessert"? poner("Dessert", desserts):  lista === "Drinks"? poner("Drinks & Sides", drinks):0;
-
-
-}
-
-
-function  poner(name, lista){
+function poner(name, lista) {
   title.innerHTML = name;
-    content.innerHTML = "";
-   
-    lista.forEach((element) => {
-      content.innerHTML += `
-      <div class= col-md-3>
-      <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="${element.image}" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">${element.name}</h5>
-    <p class="card-text">${element.description}<br>
-    <b>${element.price} </b></p>
+  content.innerHTML = "";
+
+  lista.forEach((element) => {
+    let col = document.createElement("div");
+    col.className = "col-md-3";
+
+    let card = document.createElement("div");
+    card.className = "card";
+
+
+    let cardImage = document.createElement("img");
+    cardImage.className = "card-img-top";
+    cardImage.src=element.image
+    cardImage.alt="product image"
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
     
-    <a href="#" class="btn btn-primary" onclick="addCart('${name, element.name}')">Add to Car</a>
-  </div>
-</div>
-</div>
-     `;
-    });
+    
+
+    let title = document.createElement("h5");
+    title.innerText = element.name;
+    title.className = "card-title";
+
+    let cardContent = document.createElement("p");
+    cardContent.innerText = element.description;
+    cardContent.className = "card-text";
+
+    let cardPrice = document.createElement("b");
+    cardPrice.innerText = element.price;
+    cardPrice.className = "card-text";
+
+
+    let cardButton= document.createElement("a");
+    cardButton.innerText = "Add to Cart";
+    cardButton.className = "btn btn-primary";
+    cardButton.id="addCar"
+    cardButton.addEventListener('click',function(){
+      productos.push(element)
+      total+=1
+      items.innerText=total+" items"
+    })
+
+
+    col.appendChild(card)
+    card.appendChild(cardImage)
+    card.appendChild(cardBody)
+    cardContent.appendChild(document.createElement("br"))
+    cardContent.appendChild(cardPrice)
+    cardBody.appendChild(title)
+    cardBody.appendChild(cardContent)
+    cardBody.appendChild(cardButton)
+
+    content.appendChild(col);
+    
+  });
+
+  
 }
 
+let headers=["Item", "Qty.", "Description", "Unit Price", "Amount"]
 
-function addCart(name, obj){
-  total+=1
-  items.innerHTML=total
-  toadd=""
-  console.log(name)
-  console.log(obj)
-  if (name==="Burgers"){
-    toadd=search(burgers, obj)
-  }else if (name==="Tacos"){
-    toadd=search(tacos, obj)
-  }else if (name==="Salads"){
-    toadd=search(salads, obj)
-  }else if (name==="Dessert"){
-    toadd=search(desserts, obj)
-  }else if (name==="Drinks & Sides"){
-    toadd=search(drinks, obj)
-  }
-  productos.push(toadd)
-  console.log(productos)
-}
+function pedido(){
+  toTable=purchase()
+  tab = document.createElement("table");
+  tab.className="table"
+  title.innerHTML = "Order Detail";
+  content.innerHTML = "";
+  tr= document.createElement("tr")
 
-
-function search(name, obj){
-  name.forEach(el=>{
-    console.log(el.name)
-    if(el.name===obj){
-      return el ;
-    } 
+  headers.forEach(head=>{
+    th= document.createElement("th")
+    th.innerText=head
+    tr.appendChild(th)
   })
+
+  
+  tab.appendChild(tr)
+  tot=0
+  toTable.forEach(prod=>{
+    tr= document.createElement("tr")
+    td= document.createElement("td")
+    td.innerText= tot+=1
+    tr.appendChild(td)
+    for (key in prod){
+  
+    td= document.createElement("td")
+    td.innerText= prod[key]
+    tr.appendChild(td)
+  }
+  tab.appendChild(tr)
+    
+
+  })
+
+  content.appendChild(tab)
+
+}
+
+  
+function purchase(){
+  lis=[]
+
+  productos.forEach(prod=>{
+    a=check(lis,prod.name)
+    if (a!=-1){
+      act= lis[a]
+      act["q"]+=1
+      act["val"]=prod.price*act["q"]
+      lis[a]=act
+    }else{
+      obj={"q":1, "des":prod.name, "un":prod.price, "val":prod.price}
+      lis.push(obj)
+    }
+  })
+  return lis
+}
+
+function check(lis,name){
+  ans=-1
+  for (var i=0;i<lis.length;i++){
+    act=lis[i]
+    if (act["des"]===name){
+      
+      ans= i
+    }
+  }
+  console.log(ans)
+  return ans
 }
